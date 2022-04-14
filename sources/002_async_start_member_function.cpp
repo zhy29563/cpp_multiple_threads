4eda11f0339d35d71abc5c94a36ef5c355264827
Steven Zhou <zhy29563@hotmail.com>
2022-04-14 23:25:01 +0800

    启动无参无返回值的可调用对象

---
+++ sources/002_async_start_member_function.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <future>
+#include <chrono>
+
+class A
+{
+public:
+    void function()
+    {
+        std::cout << "child thread: start" << std::endl;
+        std::cout << "child thread: id is " << std::this_thread::get_id() << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+
+        std::cout << "child thread: end" << std::endl;
+    }
+};
+
+int main()
+{
+    std::cout << "parent thread: start" << std::endl;
+    std::cout << "parent thread: id is " << std::this_thread::get_id() << std::endl;
+
+    A a;
+    std::future<void> result_future = std::async(&A::function, &a);
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "parent thread: run other task" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    result_future.wait();
+
+
+    std::cout << "parent thread: end" << std::endl;
+    return EXIT_SUCCESS;
+}
