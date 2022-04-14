a89a77ba2c581134c2c47c8122f243f0c5b3a0bd
Steven Zhou <zhy29563@hotmail.com>
2022-04-14 23:27:10 +0800

    启动有参有返回值的可调用对象

---
+++ sources/013_async_start_global_function.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <thread>
+#include <future>
+#include <chrono>
+
+int function(int i)
+{
+    std::cout << "child thread: start" << std::endl;
+    std::cout << "child thread: id is " << std::this_thread::get_id() << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    std::cout << "child thread: parameter is " << i << std::endl;
+
+    std::cout << "child thread: end" << std::endl;
+
+    return 100;
+}
+
+int main()
+{
+    std::cout << "parent thread: start" << std::endl;
+    std::cout << "parent thread: id is " << std::this_thread::get_id() << std::endl;
+
+    std::future<int> result_future = std::async(function, 200);
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "parent thread: run other task" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    int result = result_future.get();
+    std::cout << "the return is " << result << std::endl;
+
+    std::cout << "parent thread: end" << std::endl;
+    return EXIT_SUCCESS;
+}
---
+++ sources/016_async_start_lambda_expression.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+#include <future>
+#include <chrono>
+
+int main()
+{
+    std::cout << "parent thread: start" << std::endl;
+    std::cout << "parent thread: id is " << std::this_thread::get_id() << std::endl;
+
+    int i = 200;
+    std::future<int> result_future = std::async([=]()->int{
+        std::cout << "child thread: start" << std::endl;
+        std::cout << "child thread: id is " << std::this_thread::get_id() << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+
+        std::cout << "child thread: parameter is " << i << std::endl;
+
+        std::cout << "child thread: end" << std::endl;
+
+        return 100;
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "parent thread: run other task" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    int result = result_future.get();
+    std::cout << "the return is " << result << std::endl;
+
+    std::cout << "parent thread: end" << std::endl;
+    return EXIT_SUCCESS;
+}
